aecc54ff2aeb422bb2c56587fd1c1b5823ede80f
zoe <zoweet@gmail.com>
2017-12-01 16:00:17 -0700

    i changed the js

--- golf.js
+++ golf.js
@@ -1,23 +1,92 @@
 
 
 var closeCourse;
-var local_obj = {latitude: 40.4436, longitude: - 111.86311, radius: 48.2803};
+var currentCourse;
+var local_obj = {latitude: 40.4436, longitude: - 111.86311, radius: 100};
+var numHoles;
+var numplayers=4;
+
 
 function loadMe() {
-    $.post("https://golf-courses-api.herokuapp.com/courses", local_obj, function (data, status) {
+    $.post("https://golf-courses-api.herokuapp.com/courses/", local_obj, function (data, status) {
         closeCourse = JSON.parse(data);
-        for (var p in closeCourse.courses){
+        console.log(closeCourse);
+        for (let p in closeCourse.courses){
             $(".dropdown-menu").append("<div class='courselist'>" +
-                "<a href=''>"+ closeCourse.courses[p].name +"</a>" +
+                "<a onclick='informationOpening(" + p +","+ closeCourse.courses[p].id+")'> " + closeCourse.courses[p].name +"</a>" +
                 "</div>");
         }
-        $(".infoboxstuff").append("<div>"+ closeCourse.courses[n].name +"</div>");
-        $(".infoboxstuff").append("<div>"+ closeCourse.courses[n].addr_1 +"</div>");
-        $(".infoboxstuff").append("<div>"+ closeCourse.courses[n].city +', '+ closeCourse.courses[0].state_or_province + "</div>");
-        $(".infoboxstuff").append("<div>"+ closeCourse.courses[n].country +"</div>");
-        $(".infoboxstuff").append("<div>"+ closeCourse.courses[n].hole_count +"</div>");
-        $(".infoboxstuff").append("<div>"+ closeCourse.courses[n].phone+"</div>");
-
-
-    })
+    });
 }
+
+function informationOpening(p, myid) {
+    $("#infoboxstuff").empty().append("<div>" + closeCourse.courses[p].name + "</div>");
+    $("#infoboxstuff").append("<div>" + closeCourse.courses[p].addr_1 + "</div>");
+    $("#infoboxstuff").append("<div>" + closeCourse.courses[p].city + ', ' + closeCourse.courses[0].state_or_province + "</div>");
+    $("#infoboxstuff").append("<div>" + closeCourse.courses[p].country + "</div>");
+    $("#infoboxstuff").append("<div>" + closeCourse.courses[p].phone + "</div>");
+    $("#infoboxstuff").append("<div><a href='" + closeCourse.courses[p].website + "'>Website</a></div>");
+    $("#infoboxstuff").append("<div>" + closeCourse.courses[p].hole_count + ' holes' +"</div>");
+    $("#infoboxstuff").append("<div ><img class='info' src='" + closeCourse.courses[p].thumbnail + "'></div>");
+    getCourse(myid);
+
+
+}
+
+function getCourse(courseid) {
+    console.log(courseid);
+    $("#teeselect").html("");
+    $.get("https://golf-courses-api.herokuapp.com/courses/" + courseid, function (data, status) {
+        currentCourse = JSON.parse(data);
+        for (let t in currentCourse.course.tee_types) {
+            var teename = currentCourse.course.tee_types[t].tee_type;
+            console.log(teename);
+
+            $("#teeselect").append("<option value ='" + t + "'>" + teename + " </option>")
+        }
+
+    });
+
+}
+
+    function buildCard(mytee) {
+        numHoles = currentCourse.course.holes;
+        $(".scorecolumn").html("");
+        for(var c in numHoles){
+            var holepar = currentCourse.course.holes[c].tee_boxes[mytee].par;
+            console.log(holepar);
+            $(".scorecolumn").append("<div id='golumn" + (Number(c) + 1) + "'class='golumn'><div class='holeheader'><div class='parbox'>"+ (Number(c) + 1) +"</div><div>par " + holepar + "</div></div></div>");
+        }
+
+        $(".scorecolumn").append("<div class='totalc'><div>total</div></div>");
+        fillCard();
+    }
+
+
+   function fillCard() {
+       for(let p = 1; p <= numplayers; p++){
+           console.log(p);
+               $(".playercolumn").append("<div id='pl"+p+"'><span class='fa fa-minus-circle' onclick='deleteplayer("+p+")'></span>" + "<div class='person' contenteditable='true'>Player</div>");
+               $(".totalc").append("<input class='holeinput' id='totalhole" + p + "'>");
+               for(let h = 1; h <= numHoles.length; h++) {
+                   $("#golumn" + h).append("<input id='player" + p + "hole" + h + "' class='holeinput' onkeyup='updatescore(" + p + ")'>");
+
+               }
+           }
+       }
+       function deleteplayer(playerId) {
+           $("#pl" + playerId).remove();
+           for(var j = 1; j <= numHoles.length; j++){
+               $("#player" + playerid + "hole" + j).remove();
+
+           }
+    function updatescore(playerid){
+               var playertotal = 0;
+               for(var t = 1; t <= numholes.length; t++){
+                  playertotal += $("#player" + playerid + "hole" +t).val();
+               }
+    }
+
+    $("#totalhole"+playerid).val(playertotal);
+
+   }
